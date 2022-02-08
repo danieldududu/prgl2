2c2b7ef6d8bf43a1d4160ffa607cc109a4b76f55
Daniel Du <danielducanada@gmail.com>
2022-02-08 09:18:43 -0800

    Added the rest of the files including exercises.

---
+++ Feb 4 Warmup.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Feb 4 Warmup|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(+ (sqr 4) (/ 14 (* 2 3.5)) (* 12 4))
+(+ (sqr 4) (/ 14 7)         (* 12 4))
+(+ (sqr 4) (/ 14 7)              48 )
+(+  16         2                 48 )
